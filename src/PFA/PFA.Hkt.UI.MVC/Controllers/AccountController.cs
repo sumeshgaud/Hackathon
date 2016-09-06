3263afbb61ffd6edbc4690f453f0602b81d60729
sumeshgaud <sumesh.gaud@aspiresys.com>
2016-09-06 10:54:56 +0530

    Initial code

---
+++ src/PFA/PFA.Hkt.UI.MVC/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using PFA.Hkt.UI.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PFA.Hkt.UI.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Login()
+        {
+            ViewBag.success = "true";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string password, string userName)
+        {
+            ViewBag.success = "false";
+            ViewBag.message = "Username and password incorrect. Try later.";
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult SubmitRegistration(RegistrationViewModel registrationViewModel)
+        {
+            return View("Register");
+        }
+
+    }
+}

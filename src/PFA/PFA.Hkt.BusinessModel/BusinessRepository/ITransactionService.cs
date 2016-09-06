92c993c346300da22bc8bb8756fc0fae8d6ebd17
Vishal Srivastava <vishal.srivastava@aspiresys.com>
2016-09-06 12:42:57 +0530

    Commit Repository

---
+++ src/PFA/PFA.Hkt.BusinessModel/BusinessRepository/ITransactionService.cs
@@ -0,0 +1,19 @@
+﻿using BusinessModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessModel
+{
+    public interface ITransactionService
+    {
+        beTransaction GetTransactionById(Guid transId);
+        IEnumerable<beTransaction> GetAllTransaction();
+        Guid CreateTransaction(beTransaction transEntity);
+        bool UpdateTransaction(Guid transId, beTransaction transEntity);
+        bool DeleteTransaction(Guid transId);
+
+    }
+}
---
+++ src/PFA/PFA.Hkt.BusinessModel/BusinessRepository/TransactionService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using DataModel;
+using AutoMapper;
+using BusinessModel;
+
+namespace BusinessModel
+{
+    public class TransactionService : ITransactionService
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public TransactionService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<beAccount> GetAccountsByUserId(Guid userId)
+        {
+            Func<Account, bool> predicate = (p => p.UserId == userId);
+            var accountModel = _unitOfWork.AccountRepository.GetMany(predicate).ToList();
+            if (accountModel != null)
+            {
+                Mapper.CreateMap<Account, beAccount>();
+
+                var userAccount = Mapper.Map<List<Account>, List<beAccount>>(accountModel);
+                return userAccount;
+            }
+            return null;
+        }
+
+        public IEnumerable<beTransaction> GetAllTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid CreateTransaction(beTransaction transEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateTransaction(Guid transId, beTransaction transEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteTransaction(Guid transId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public beTransaction GetTransactionById(Guid transId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
